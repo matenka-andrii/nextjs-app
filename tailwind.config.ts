4ce1e623c80520131e7388165e9dab64b16aeeed
Andrii Matenka <matenka.andrii@gmail.com>
2025-08-14 12:10:04 +0300

    added auth route using nextauth; added theme's variables, fonts; configured root layout

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+// tailwind.config.mjs
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+};
+export default config;
