81c2a62f61ff0173f63e38a486d6a9588b2f971b
Andrii Matenka <matenka.andrii@gmail.com>
2025-08-27 10:12:26 +0300

    created StartupCard.tsx component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,33 @@
+// Components
 import SearchForm from '@/components/SearchForm';
+import StartupCard from '@/components/StartupCard';
 
 export default async function Home({ searchParams }: {
     searchParams: Promise<{ query?: string }>;
 }) {
     const query = ( await searchParams ).query;
+
+    /* Html */
+    const getPosts = () => {
+        const posts = [{
+            _id: 1,
+            _createdAt: new Date(),
+            views: 55,
+            author: { _id: 1, name: 'Andrii' },
+            description: 'This is a description',
+            image: 'https://cdn.pixabay.com/photo/2022/01/07/10/38/playstation-controller-6921619_1280.jpg',
+            category: 'Playstation',
+            title: 'We Love Playstation',
+        }];
+
+        if (posts?.length > 0) {
+            return posts.map((post: StartupCardType) => (
+                <StartupCard key={post?._id} post={post} />
+            ));
+        }
+
+        return <p className='no-result'>No startups found</p>;
+    };
 
     return (
         <>
@@ -16,6 +40,15 @@ export default async function Home({ searchParams }: {
                     Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
                 </p>
                 <SearchForm query={query}/>
+            </section>
+            <section className='section_container'>
+                <p className="text-30-semibold">
+                    { query ? `Search results for "${query}"` : 'All Startups' }
+                </p>
+                
+                <ul className="mt-7 card_grid">
+                    { getPosts() }
+                </ul>
             </section>
         </>
     );
